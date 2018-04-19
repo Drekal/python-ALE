0abf0c90f65854f768f1c699d81a86d849e171a8
Drekal <bwhaa@live.fr>
2018-04-19 14:45:05 +0200

    TP

---
+++ Python-ALE.py
@@ -0,0 +1,109 @@
+# def exo1():
+#     for i in range(500):
+#         print("Je dois faire des sauvegardes regulieres de mes fichiers.")
+#
+# exo1()
+#
+# #----------------------------
+#
+# def exo2():
+#     count = 2
+#     for i in range(1, 1000, 2):
+#         print(i)
+#
+# exo2()
+#
+# #----------------------------
+#
+# def exo3():
+#     for i in range(1, 11):
+#         print(i, "x 13 =", i * 13)
+#
+# exo3()
+#
+# #----------------------------
+#
+# def exo4():
+#     i = input("Give me a word : ")
+#     print (len(i))
+#
+# exo4()
+#
+# # ----------------------------
+#
+# def exo5():
+#     i = input("Give me an integer : ")
+#     if int(i) % 2 == 0:
+#         print("This number is even")
+#     else:
+#         print("This number is odd")
+#
+# exo5()
+#
+# #----------------------------
+# #!!!
+# def exo6():
+#     i = input("Give me a number between 10 and 20 : ")
+#
+#     if int(i) > int(10) and int(i) < int(20):
+#         print("Good")
+#     elif int(i) > int(20):
+#         print("Lesser")
+#     elif int(i) < int(10):
+#         print("Greater")
+#
+# exo6()
+#
+# #----------------------------
+# #!!!
+# def exo7():
+#     a = 0
+#     i = input("Give me a number : ")
+#     while a < 11:
+#         i = i + 1
+#         print(i)
+#
+# exo7()
+#
+# #----------------------------
+#
+# def exo8():
+#     a = input("Table ? ")
+#
+#     for i in range(1, 11):
+#         print(i, "x", int(a), "=", i * int(a))
+#
+# exo8()
+#
+# #----------------------------
+# #!!!
+# def exo9():
+#     i = input("Give me a number")
+#
+# exo9()
+#
+# #----------------------------
+#
+# def exo10():
+#     i = int(input("Age of the children : "))
+#
+#     if i == 6 or i == 7:
+#         print("Poussin")
+#     elif i == 8 or i == 9:
+#         print("Pupille")
+#     elif i == 10 or i == 11:
+#         print("Minime")
+#     elif i > 11:
+#         print("Cadet")
+#     else:
+#         print("Too young !")
+#
+# exo10()
+#
+# #----------------------------
+# #!!!
+# def exo11():
+#
+#
+# exo11()
+
